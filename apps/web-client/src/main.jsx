522d9d0e32f33c0af3bb410bb01a7209f8007c40
Justin Andrew Baker <justinabsentia@gmail.com>
2025-12-13 05:50:06 +0000

    failed to resolve import

--- apps/web-client/src/main.jsx
+++ apps/web-client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import EntangleduMain from './EntangleduMain'
+import EntangleduMain from './EntangleduMain.jsx'
 import './index.css' // We will create this in Step 3
 
 ReactDOM.createRoot(document.getElementById('root')).render(
