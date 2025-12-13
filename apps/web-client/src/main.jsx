d7c5f9d6643bc61352d9e9f0c83ca62fe1f9c83c
Justin Andrew Baker <justinabsentia@gmail.com>
2025-12-13 05:44:35 +0000

    5 changes. forgot the chasis

---
+++ apps/web-client/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import EntangleduMain from './EntangleduMain'
+import './index.css' // We will create this in Step 3
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <EntangleduMain />
+  </React.StrictMode>,
+)
